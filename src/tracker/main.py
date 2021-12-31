12b22bc8f0ca4964f4ed3c503dc6cafdb6e02906
Connor McCarthy <connor15mcc@gmail.com>
2021-12-30 21:45:29 -0500

    added removal of pickle when testing, test a distinct pickle

--- src/tracker/main.py
+++ src/tracker/main.py
@@ -1,16 +1,23 @@
 from typing import List
 from tracker.tracker import Tracker
 import pickle
+import os
+
+PICKLE_LOCATION = "tracker.p"
 
 
-def unpickle_trackers() -> List[Tracker]:
+def unpickle_trackers(loc: str) -> List[Tracker]:
     try:
-        with open("tracker.p", "rb") as pickler:
+        with open(loc, "rb") as pickler:
             return pickle.load(pickler)
     except Exception:
         return []
 
 
-def pickle_trackers(trkrs: List[Tracker]) -> None:
-    with open("tracker.p", "wb") as pickler:
+def pickle_trackers(loc: str, trkrs: List[Tracker]) -> None:
+    with open(loc, "wb") as pickler:
         pickle.dump(trkrs, pickler)
+
+
+def remove_pickle(loc: str) -> None:
+    os.remove(loc)
