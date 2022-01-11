6f4c82aba21fa526cc4105ed46999b24426c1c62
Connor McCarthy <connor15mcc@gmail.com>
2022-01-10 21:52:26 -0500

    fixed acquisition of env keys

--- src/tracker/config.py
+++ src/tracker/config.py
@@ -1,13 +1,17 @@
+from dotenv import load_dotenv
 import os
 
 
+load_dotenv()
+
+
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = "sqlite:///tracker.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ConfigTest:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = "sqlite:///tracker-test.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
