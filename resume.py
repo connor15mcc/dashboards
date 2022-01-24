d2d330b14af80f12851402adbe932eec4f5b6db3
Connor McCarthy <connor15mcc@gmail.com>
2022-01-24 17:49:14 -0500

    fixed tracking of pdf coverletter

--- resume.py
+++ resume.py
@@ -1,10 +1,7 @@
-from email import header
 import yaml
 import jinja2
 import os
 import subprocess
-import tempfile
-import shutil
 
 
 DATA = "data.yaml"
@@ -116,8 +113,36 @@ def clean_all_aux():
             os.remove(file)
 
 
-if __name__ == "__main__":
+def main():
     fill_all_templates()
     render_all_templates()
     clean_all_build()
     clean_all_aux()
+
+
+def update_coverletter(new_name, new_address1, new_address2):
+    with open(DATA) as f:
+        content = yaml.safe_load(f)
+
+    content["coverletter"]["name"] = new_name
+    content["coverletter"]["address1"] = new_address1
+    content["coverletter"]["address2"] = new_address2
+
+    with open(DATA, "w") as f:
+        yaml.dump(content, f, sort_keys=False)
+
+    fill_all_templates()
+    render_latex(COVERLETTER_FILLED, COVERLETTER_OUTPUT)
+    clean_all_build()
+    clean_all_aux()
+
+    content["coverletter"]["name"] = "Company"
+    content["coverletter"]["address1"] = "123 Constitution Lane"
+    content["coverletter"]["address2"] = "New York, NY 12345"
+
+    with open(DATA, "w") as f:
+        yaml.dump(content, f, sort_keys=False)
+
+
+if __name__ == "__main__":
+    main()
